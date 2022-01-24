39b502400f30dd8eaf6a76b49d48ea3618f6e5f0
clsong <chuliangsong1994@gmail.com>
2022-01-23 21:57:28 -0500

    update plot method

--- R/plot_spatial_association.R
+++ R/plot_spatial_association.R
@@ -1,92 +1,91 @@
+# plot_spatial_association <- function(spatial_association, plot_type = 'ggraph'){
+#   if(plot_type == 'ggraph'){
+#     p <- plot_spatial_association_ggraph(spatial_association)
+#   }
+#   # if(plot_type == 'DiagrammeR'){
+#   #   p <- plot_spatial_association_DiagrammeR(spatial_association)
+#   # }
+#
+#
+#   p
+# }
+
+#' #' @import DiagrammeR
+#' plot_spatial_association_DiagrammeR <- function(spatial_association){
+#'   length <- .8
+#'   n <- tibble(
+#'     node = LETTERS[1:gamma]
+#'   ) %>%
+#'     mutate(
+#'       label = node
+#'     ) %>%
+#'     rename(
+#'       id = node
+#'     ) %>%
+#'     mutate(
+#'       width = .3,
+#'       height = .3,
+#'       alpha = 354.91
+#'     )
+#'
+#'   e <- spatial_association %>%
+#'     as.table() %>%
+#'     as.data.frame() %>%
+#'     transmute(
+#'       from = Var1,
+#'       to = Var2,
+#'       weight = Freq
+#'     ) %>%
+#'     mutate(
+#'       association = case_when(
+#'         weight > 0 ~ "attractive",
+#'         weight == 0 ~ "null",
+#'         weight < 0 ~ "repulsive"
+#'       )
+#'     ) %>%
+#'     filter(
+#'       match(from, LETTERS[1:gamma]) <= match(to, LETTERS[1:gamma])
+#'     ) %>%
+#'     mutate(from = factor(from), to = factor(to)) %>%
+#'     mutate(color = case_when(
+#'       association == 'null' ~ "gray",
+#'       association == 'repulsive' ~ "#4594B8",
+#'       association == 'attractive' ~ "#8E5750",
+#'       TRUE ~ 'white'))  %>%
+#'     mutate(arrowhead = case_when(
+#'       weight < 0 ~ "inv",
+#'       weight == 0 ~ "none",
+#'       weight > 0 ~ "normal"
+#'     )) %>%
+#'     mutate(
+#'       arrowtail = arrowhead,
+#'       fontcolor = color,
+#'       dir = 'both',
+#'       penwidth = ifelse(weight == 0, .5,  abs(weight)),
+#'       label = weight)
+#'
+#'   create_graph() %>%
+#'     add_nodes_from_table(
+#'       table = n
+#'     ) %>%
+#'     add_edges_from_table(
+#'       table = e,
+#'       from_col = from,
+#'       to_col = to,
+#'       from_to_map = id_external
+#'     ) %>%
+#'     set_node_attr_to_display(attr = id_external) %>%
+#'     render_graph()
+#' }
+
 #' Plot the spatial associations
 #'
 #' @return A graph visualizing spatial associations
 #' @spatial_association a matrix with elements encoding the strength of species associations. Strength in (0, 1) indicates attraction, = 0 indicates null relationship, in (-1, 0) indicates repulsion
 #' @plot_type Either using the package ggraph or DiagrammR
 #' @export
-
-plot_spatial_association <- function(spatial_association, plot_type = 'ggraph'){
-  if(plot_type == 'ggraph'){
-    p <- plot_spatial_association_ggraph(spatial_association)
-  }
-  if(plot_type == 'DiagrammeR'){
-    p <- plot_spatial_association_DiagrammeR(spatial_association)
-  }
-
-
-  p
-}
-
-#' @import DiagrammeR
-plot_spatial_association_DiagrammeR <- function(spatial_association){
-  length <- .8
-  n <- tibble(
-    node = LETTERS[1:gamma]
-  ) %>%
-    mutate(
-      label = node
-    ) %>%
-    rename(
-      id = node
-    ) %>%
-    mutate(
-      width = .3,
-      height = .3,
-      alpha = 354.91
-    )
-
-  e <- spatial_association %>%
-    as.table() %>%
-    as.data.frame() %>%
-    transmute(
-      from = Var1,
-      to = Var2,
-      weight = Freq
-    ) %>%
-    mutate(
-      association = case_when(
-        weight > 0 ~ "attractive",
-        weight == 0 ~ "null",
-        weight < 0 ~ "repulsive"
-      )
-    ) %>%
-    filter(
-      match(from, LETTERS[1:gamma]) <= match(to, LETTERS[1:gamma])
-    ) %>%
-    mutate(from = factor(from), to = factor(to)) %>%
-    mutate(color = case_when(
-      association == 'null' ~ "gray",
-      association == 'repulsive' ~ "#4594B8",
-      association == 'attractive' ~ "#8E5750",
-      TRUE ~ 'white'))  %>%
-    mutate(arrowhead = case_when(
-      weight < 0 ~ "inv",
-      weight == 0 ~ "none",
-      weight > 0 ~ "normal"
-    )) %>%
-    mutate(
-      arrowtail = arrowhead,
-      fontcolor = color,
-      dir = 'both',
-      penwidth = ifelse(weight == 0, .5,  abs(weight)),
-      label = weight)
-
-  create_graph() %>%
-    add_nodes_from_table(
-      table = n
-    ) %>%
-    add_edges_from_table(
-      table = e,
-      from_col = from,
-      to_col = to,
-      from_to_map = id_external
-    ) %>%
-    set_node_attr_to_display(attr = id_external) %>%
-    render_graph()
-}
-
 #' @import ggraph tidygraph
-plot_spatial_association_ggraph <- function(spatial_association){
+plot_spatial_association <- function(spatial_association){
   spatial_association %>%
     as.table() %>%
     as.data.frame() %>%
